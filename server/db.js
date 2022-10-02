afe6f6fb757f9b750394d21c49680bea73dca2d8
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-03 00:04:13 +0300

    Connection to db

--- server/db.js
+++ server/db.js
@@ -1,1 +1,17 @@
-const { Sequiloize } = require('sequelize')
+const Sequilize = require('sequelize')
+
+module.exports = new Sequilize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST,
+        pool: {
+            max: 100,
+            min: 0,
+            idle: 200000,
+            acquire: 1000000,
+          },
+    }
+)
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 const express = require('express')
-
+const sequelize = require('./db')
 const PORT = process.env.PORT || 3000
-
+const models = require('./models/models')
 const app = express()
 
-app.listen(PORT, () => console.log('Server is running on port ' + PORT))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
