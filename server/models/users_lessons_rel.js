6c9c88ed008457fb1e1da159be3b59315dac2507
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-03 23:37:45 +0300

    Refactor gitignore

--- server/models/users_lessons_rel.js
+++ server/models/users_lessons_rel.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize')
 const users_lessons_rel = sequelize.define('users_lessons_rel', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     user_id: { type: DataTypes.INTEGER, allowNull: false },
-    lesson_id: { type: DataTypes.INTEGER, allowNull: false },
+    lesson_id: { type: DataTypes.INTEGER, allowNull: false }
 })
 
 module.exports = users_lessons_rel
