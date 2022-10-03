f382faffe4faab48b1c27631eabec1d06b4b9a8c
Ruslan Tsykin <gamerule723@gmail.com>
2022-10-03 23:32:14 +0300

    added lesson_types model

--- server/models/models.js
+++ server/models/models.js
@@ -1,9 +1,11 @@
 const { users } = require('./users')
 const { lessons } = require('./lessons')
+const { lesson_types } = require('./lesson_types')
 const { users_lessons_rel } = require('./users_lessons_rel')
 
 module.exports = {
   users,
   lessons,
+  lesson_types,
   users_lessons_rel
 }
