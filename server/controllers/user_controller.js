bf3caa5ce2b5dfde976a62f9d6872db2b5c8275f
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-04 23:00:21 +0300

    revomed class coz we use functions

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,18 +1,10 @@
 const db = require('../db')
 
-class UserController {
-    async GetUsers(req, res){                
-        const users = await db.query('SELECT * from users')
-        res.json(users)
-    }
+async function GetUsers (req, res) {                
+    const users = await db.query('SELECT * from users')
+    res.json(users)
 }
 
-
-
-// async function GetUsers (res, req, body, next)  {
-    
-//     const users = await db.query(`SELECT * from users`)
-//     res.send(users)
-// }
-
-module.exports = new UserController()
+module.exports = {
+    GetUsers,
+}
