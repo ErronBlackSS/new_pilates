7ff7203087e9a6d7f2caa05db7ec6d4dc7ebeb4f
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-05 02:10:30 +0300

    rename db to pool

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,27 +1,27 @@
-const db = require('../db')
+const pool = require('../db')
 const helpers = require('../helpers/general')
 
 async function Get (req, res) {                
-    const users = await db.query('SELECT * from users')
+    const users = await pool.query('SELECT * from users')
     res.json(users.rows)
 }
 
 async function Create (req, res) {
     const { name, email, phone, password, lastname } = req.body
     console.log(req.body)
-    const newUser = await db.query('INSERT INTO users (name, email, phone, password, lastname) VALUES ($1, $2, $3, $4, $5)', [name, email, phone, password, lastname])
+    const newUser = await pool.query('INSERT INTO users (name, email, phone, password, lastname) VALUES ($1, $2, $3, $4, $5)', [name, email, phone, password, lastname])
     res.json(newUser.rows[0])
 }
 
 async function Update (req, res) {
     const query = helpers.parseUpdateData(req.body, 'users')
-    const user = await db.query(query, [])
+    const user = await pool.query(query, [])
     res.json(user.rows[0])
 }
 
 async function Delete (req, res) {
     const { id } = req.body
-    const user = await db.query('DELETE FROM users WHERE id = $1', [id])
+    const user = await pool.query('DELETE FROM users WHERE id = $1', [id])
     res.json(user.rows[0])
 }
 
