87f41ce8605a7b7bd0a5dfedd0bf3ea215698bda
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-11-11 23:37:58 +0300

    Сделал форму создания занятия

--- client/src/components/Common/Select.tsx
+++ client/src/components/Common/Select.tsx
@@ -18,17 +18,22 @@ const Select = ({ options, label, onSelect }) => {
   const rounded = (index) => index === 0 ? ' rounded-t-[10px]' : index === options.length-1 ? ' rounded-b-[10px]' : ''
 
   return (
-    <div className="relative">
+    <div
+      className="relative"
+      ref={list}
+    >
       <label
         className="block ml-[10px] text-[12px] leading-[15px] text-[#000000]"
       >
         {label}
       </label>
-      <div className="flex items-center px-[15px] text-[16px] h-[50px] w-[350px] bg-[#F2F2F3] rounded-[10px]">
-        <div className="flex flex-row justify-between items-center w-full">
+      <div
+        onClick={() => setOpened(!opened)}
+        className="flex items-center px-[15px] text-[16px] h-[50px] w-[350px] bg-[#F2F2F3] cursor-pointer rounded-[10px]"
+      >
+        <div className="flex flex-row cursor-pointer justify-between items-center w-full">
           <input value={value.label} type="text" disabled />
           <svg
-            onClick={() => setOpened(!opened)}
             className="cursor-pointer"
             width="15"
             height="8"
@@ -46,14 +51,13 @@ const Select = ({ options, label, onSelect }) => {
         </div>
       </div>
       {opened && <ul
-        ref={list}
         className="mt-[5px] absolute top-[100%] w-[350px] transition duration-300 max-h-[300px] overflow-y-auto overflow-x-hidden"
         style={{ zIndex: 100 }}
       >
         {options.map((option, index) => (
           <li
             key={option.value}
-            className={'flex px-[15px] items-center text-[16px] h-[50px] w-[350px] align-center bg-[#F2F2F3] text-left cursor-pointer' + rounded(index)}
+            className={'flex px-[15px] items-center text-[16px] h-[50px] w-[350px] align-center bg-[#FFF] text-left cursor-pointer' + rounded(index)}
             onClick={() => handleSelect(option)}
           >
             <p>{option.label}</p>
--- client/src/components/Forms/AddLessonForm.tsx
+++ client/src/components/Forms/AddLessonForm.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import Select from '../Common/Select'
+import { TimePicker } from 'react-ios-time-picker'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 const AddLessonForm = ({ trainers, lessonTypes }) => {
   
   const [trainer, setTrainer] = useState({ value: '', label: '' })
   const [lessonType, setLessonType] = useState({ value: '', label: '' })
-  
+  const [startTime, setStartTime] = useState('10:00')
+  const [endTime, setEndTime] = useState('10:00')
+  const [startDate, setStartDate] = useState(new Date())
+
+  const formDisabled = !trainer.value || !lessonType.value || !startTime || !endTime || !startDate
+
   const onSubmit = (value) => {
     console.log(trainer, lessonType, value)
   }
@@ -20,7 +28,28 @@ const AddLessonForm = ({ trainers, lessonTypes }) => {
         <div className="flex flex-col gap-[25px]">
           <Select options={trainers} label="Тренер" onSelect={setTrainer}/>
           <Select options={lessonTypes} label="Тип занятия" onSelect={setLessonType}/>
+          <div className="flex flex-row gap-[30px]">
+            <TimePicker
+              onSave={setStartTime}
+              value={startTime}
+              saveButtonText="Сохранить"
+              cancelButtonText="Отменить"
+            />
+            <TimePicker
+              onSave={setEndTime}
+              value={endTime}
+              saveButtonText="Сохранить"
+              cancelButtonText="Отменить"
+            />
+          </div>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
         </div>
+        <button
+          disabled={formDisabled}
+          className={ 'w-[100%] px-6 py-2 mt-4 text-[#fff] cursor-pointer rounded-[10px] ' + (formDisabled ? ' bg-[#D11655] opacity-40' : 'bg-bordo')}
+        >
+          Создать тип занятия
+        </button>
       </form>
     </div>
   )
