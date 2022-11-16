8cab1b80865fa06f89c4ddc5fd5513b3abd91999
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-11-17 01:10:44 +0300

    Фикс warninga + начал верстку страницы профиля

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -1,9 +1,23 @@
+import { observer } from 'mobx-react-lite'
+
 const Profile = () => {
+
+
+
   return (
-    <div className="items-center flex flex-col justify-center h-screen w-full bg-[#ea8df7]">
-      ТУТ БУДУТ НАСТРОЙКИ АККАУНТА   
+    <div className="ml-[300px] bg-[#FEFAFA]">
+      <div className="flex flex-col pt-[50px]">
+        <span className="text-[36px] leading-[56px] text-[#1B1B1B] mobile-below:text-[22px] mobile-below:leading-[34px]">
+          Личные данные
+        </span>
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+
+          </div>
+        </div>
+      </div>  
     </div>
   )
 }
 
-export default Profile
+export default observer(Profile)
