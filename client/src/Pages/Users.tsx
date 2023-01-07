2173e58556e9c013155375a5f83bfd7f75d1c28c
Mikhail Tovt <mikhail.tovt.02@mail.com>
2023-01-07 13:07:30 +0300

    мобильный навбар в аккаунте

--- client/src/Pages/Users.tsx
+++ client/src/Pages/Users.tsx
@@ -10,7 +10,7 @@ const Users = () => {
 
   return (
     <>
-      <div className="pt-[50px] pr-[60px] mobile-below:pr-[16px]">
+      <div className="mobile-above:pt-[50px] mobile-above:pr-[60px]">
         <div className="flex flex-row justify-between items-center">          
           <span className="text-[36px] leading-[56px] text-[#1B1B1B] mobile-below:text-[22px] mobile-below:leading-[34px]">
             {title}
